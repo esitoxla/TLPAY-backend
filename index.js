faeee46878caee1b2f8915e666ca3b5102e4803b
Irene Toxla <toxlairene@gmail.com>
2025-11-09 12:07:21 +0000

    updated index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "https://tl-pay.netlify.app/"];
+const allowedOrigins = ["http://localhost:5173", "https://tl-pay.netlify.app"];
 
 app.use(
   cors({
